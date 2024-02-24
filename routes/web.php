ba23d8f73d4067ee4e7090e976cbd2f406a68fa6
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-02-24 21:40:38 +0600

    login logout middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register',[UserController::class, 'loadRegister']);
-Route::post('/register',[UserController::class, 'register'])->name('register');
-Route::get('/referral-register',[UserController::class, 'loadReferralRegister'])->name('load.register');
-Route::get('/email-verification/{token}',[UserController::class, 'emailVerification'])->name('register.emailVerification');
+Route::group(['middleware'=>['is_logout']],function(){
+    Route::get('/register',[UserController::class, 'loadRegister']);
+    Route::post('/register',[UserController::class, 'register'])->name('register');
+    Route::get('/referral-register',[UserController::class, 'loadReferralRegister'])->name('load.register');
+    Route::get('/email-verification/{token}',[UserController::class, 'emailVerification'])->name('register.emailVerification');
+    Route::get('/login',[UserController::class,'loadLogin']);
+    Route::post('/login',[UserController::class,'userLogin'])->name('login');
+});
 
-Route::get('/login',[UserController::class,'loadLogin']);
-Route::post('/login',[UserController::class,'userLogin'])->name('login');
+Route::group(['middleware'=>['is_login']],function(){
+    Route::get('/dashboard',[DashboardController::class,'index']);
+});
